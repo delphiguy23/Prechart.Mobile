764abb7c2010b140225e43f6fb9c440224cb233a
Randy Adanza <randy@prechart.onmicrosoft.com>
2022-11-29 01:02:28 +0000

    Merged PR 180: Task 387: Update Landing Page Logo
    update landing page
    
    Related work items: #387

--- lib/views/homeView.dart
+++ lib/views/homeView.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:lottie/lottie.dart';
 import 'package:prechart_mobile/views/navigation.dart';
 
@@ -15,8 +16,22 @@ class HomeView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: Center(
-          child: Lottie.asset('assets/animations/tax-msm.json', repeat: true, reverse: true, animate: true)),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: SvgPicture.asset('assets/logo/logo.svg',
+              alignment: Alignment.center,
+              fit: BoxFit.fill,
+              width: MediaQuery.of(context).size.width * 0.8,),
+          ),
+
+          Center(
+              child: Lottie.asset('assets/animations/tax-msm.json', repeat: true, reverse: true, animate: true)),
+        ],
+      ),
     );
   }
 }
